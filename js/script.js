69afad872a5ab0343c987d34b26a38eaf7a094ce
Kvng-Divad <101060511+Kvng-Divad@users.noreply.github.com>
2022-05-07 15:06:29 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var Typed = new Typed(".typing", {
+    strings:["[Full Stack Developer]", "[Back-end Developer]", "[Front-end Developer]", "[Graphic Designer]","[Blogger / Youtuber e.t.c]",],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
